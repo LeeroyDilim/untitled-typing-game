bdbbf0edb43a05ce5fde8ec2e95746f28954fcab
LeeroyDilim <leeroydilim@gmail.com>
2026-09-11 21:25:54 -0500

    shortened variable name

--- systems/typing_buffer/typing_buffer.gd
+++ systems/typing_buffer/typing_buffer.gd
@@ -1,17 +1,17 @@
 extends Node
 
 var curr_input : String = ""
-signal input_received(input: String, character_removed: bool)
+signal input_received(input: String, char_removed: bool)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not event is InputEventKey or not event.is_pressed():
 		return
 
 	var event_key = event as InputEventKey
-	var character_removed = false
+	var char_removed = false
 
 	if event_key.keycode == KEY_BACKSPACE:
-		character_removed = true
+		char_removed = true
 		if event.is_command_or_control_pressed():
 			clear_input()
 		elif not curr_input.is_empty():
@@ -20,7 +20,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		curr_input += String.chr(event.unicode)
 
 	curr_input = curr_input.strip_edges(true,false).to_lower() #remove leading whitespace and convert to lower case
-	input_received.emit(curr_input, character_removed)
+	input_received.emit(curr_input, char_removed)
 
 func clear_input():
 	curr_input = "" 
